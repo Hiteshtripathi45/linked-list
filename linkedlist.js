19f5fef30160f60ebe319ac353c6222fd12acc64
hitesh <hiteshtripathi91@gmail.com>
2024-11-17 19:46:21 +0530

    prepand function for adding value at start

--- linkedlist.js
+++ linkedlist.js
@@ -1,6 +1,18 @@
-export class Node{
-    constructor(value){
-        this.value=value || null
-        this.nextnode=null
+import { Node } from "./node";
+
+export class linkedlist{
+    constructor(){
+        this.linkhead = null
+    }
+
+    prepend(newnode){
+        let temp = this.linkhead
+        if(this.linkhead==null){
+            this.linkhead= new Node(newnode)
+        }
+        else{
+        this.linkhead = new Node(newnode)
+        this.linkhead.next=temp
+        }
     }
 }
