744514b80a90acb42b395b4021eb5bb3942c18d2
hitesh <hiteshtripathi91@gmail.com>
2024-11-19 01:45:36 +0530

    main file to test every thing

---
+++ index.js
@@ -0,0 +1,16 @@
+import { linkedlist } from "./linkedlist.js";
+
+const list = new linkedlist
+
+list.append(5)
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+list.append(7)
+list.isstring()
+list.size()
+list.head()
